feabe28bfa341bb805ee657698ddd2c35841aea8
lmh <hugue_liu@yahoo.com>
2019-02-23 22:15:54 +0800

    代码运行时间统计 c++&python

---
+++ 统计时间/demo.cpp
@@ -0,0 +1,14 @@
+/*
+* 适用于c++11及以上版本
+*/
+#include <chrono>
+using namespace std::chrono;
+int main(){
+    auto start = steady_clock::now();
+    // your code
+    auto end = steady_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    // 浮点数，单位为ms
+    double t = double(duration.count()) * microseconds::period::num / microseconds::period::den;
+    return 0; 
+}
---
+++ 统计时间/demp.py
@@ -0,0 +1,7 @@
+import time
+
+start =time.clock()
+# your code
+end = time.clock()
+# 浮点数，单位为s
+t = end-start
